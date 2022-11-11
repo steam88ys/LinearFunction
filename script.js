8d4ab64a4859b8aea6712b88b4d23c1319f1576f
steam88ys <yssteam88@gmail.com>
2022-11-11 09:56:14 +0900

    학습용 데이터 준비(json -> tensor)

--- script.js
+++ script.js
@@ -14,6 +14,61 @@ async function getData() {
     return cleaned;
 }
 
+function createModel() {
+    // Create a sequential model
+    const model = tf.sequential();
+
+    // Add a single input layer
+    model.add(tf.layers.dense({inputShape: [1], units: 1, useBias: true}));
+
+    // Add an output layer
+    model.add(tf.layers.dense({units: 1, useBias: true}));
+
+    return model;
+}
+
+/**
+ * Convert the input data to tensors that we can use for machine
+ * learning. We will also do the important best practices of _shuffling_
+ * the data and _normalizing_ the data
+ * MPG on the y-axis.
+ */
+function convertToTensor(data) {
+    // Wrapping these calculations in a tidy will dispose any
+    // intermediate tensors.
+
+    return tf.tidy(() => {
+        // Step 1. Shuffle the data
+        tf.util.shuffle(data);
+
+        // Step 2. Convert data to Tensor
+        const inputs = data.map(d => d.horsepower)
+        const labels = data.map(d => d.mpg);
+
+        const inputTensor = tf.tensor2d(inputs, [inputs.length, 1]);
+        const labelTensor = tf.tensor2d(labels, [labels.length, 1]);
+
+        //Step 3. Normalize the data to the range 0 - 1 using min-max scaling
+        const inputMax = inputTensor.max();
+        const inputMin = inputTensor.min();
+        const labelMax = labelTensor.max();
+        const labelMin = labelTensor.min();
+
+        const normalizedInputs = inputTensor.sub(inputMin).div(inputMax.sub(inputMin));
+        const normalizedLabels = labelTensor.sub(labelMin).div(labelMax.sub(labelMin));
+
+        return {
+            inputs: normalizedInputs,
+            labels: normalizedLabels,
+            // Return the min/max bounds so we can use them later.
+            inputMax,
+            inputMin,
+            labelMax,
+            labelMin,
+        }
+    });
+}
+
 async function run() {
     // Load and plot the original input data that we are going to train on.
     const data = await getData();
@@ -33,6 +88,9 @@ async function run() {
     );
 
     // More code will be added below
+    // Create the model
+    const model = createModel();
+    tfvis.show.modelSummary({name: 'Model Summary'}, model);
 }
 
 document.addEventListener('DOMContentLoaded', run);
